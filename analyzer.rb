8fb1a885b172ccd91d5edddf2d262131146a5865
Alejandro Guevara <alejandro.guevara@thincode.com>
2018-08-27 15:32:00 -0500

    Keywords percentage feature added

--- analyzer.rb
+++ analyzer.rb
@@ -1,5 +1,6 @@
 lines = File.readlines("text.txt")
 text = lines.join
+stopwords = %w{the a by on for of are with just but and to the my i has some in}
 
 line_count = lines.size
 total_characters = text.size
@@ -7,6 +8,8 @@ total_characters_nospaces = text.gsub(/\s+/, '').size
 word_count = text.split.size
 sentence_count = text.split(/\.|\?|!/).size
 paragraph_count = text.split(/\n\n/).size
+keywords_count = text.split.select {|word| !stopwords.include?(word)}.size
+keywords_percentage = ((keywords_count.to_f / word_count.to_f) * 100).to_i
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
@@ -16,3 +19,4 @@ puts "#{sentence_count} sentences"
 puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
+puts "#{keywords_percentage}% of words are non-fluff words"
