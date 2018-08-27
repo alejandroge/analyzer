900333b3c8d89a1c8cae75e597761ce1bdff8e2f
Alejandro Guevara <alejandro.guevara@thincode.com>
2018-08-27 12:59:42 -0500

    Counting characters without blank space feature added

--- analyzer.rb
+++ analyzer.rb
@@ -1,8 +1,11 @@
 lines = File.readlines("text.txt")
-line_count = lines.size
 text = lines.join
 
+line_count = lines.size
 puts "#{line_count} lines"
 
 total_characters = text.size
 puts "#{total_characters} characters"
+
+total_characters_nospaces = text.gsub(/\s+/, '').size
+puts "#{total_characters_nospaces} characters (excluding spaces)"
