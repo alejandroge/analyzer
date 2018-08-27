c36c69bab3d02731032534ea66ddccfd9a4992cd
Alejandro Guevara <alejandro.guevara@thincode.com>
2018-08-27 15:20:16 -0500

    Tiding up the code for final version (basic features)

--- analyzer.rb
+++ analyzer.rb
@@ -2,22 +2,17 @@ lines = File.readlines("text.txt")
 text = lines.join
 
 line_count = lines.size
-puts "#{line_count} lines"
-
 total_characters = text.size
-puts "#{total_characters} characters"
-
 total_characters_nospaces = text.gsub(/\s+/, '').size
-puts "#{total_characters_nospaces} characters (excluding spaces)"
-
 word_count = text.split.size
-puts "#{word_count} words"
-
 sentence_count = text.split(/\.|\?|!/).size
-puts "#{sentence_count} sentences"
-
 paragraph_count = text.split(/\n\n/).size
-puts "#{paragraph_count} paragraphs"
 
+puts "#{line_count} lines"
+puts "#{total_characters} characters"
+puts "#{total_characters_nospaces} characters (excluding spaces)"
+puts "#{word_count} words"
+puts "#{sentence_count} sentences"
+puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
