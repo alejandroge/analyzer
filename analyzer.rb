44cfcc3a7b4fff3eaf0e7ff0199d7699cd57197f
Alejandro Guevara <alejandro.guevara@thincode.com>
2018-08-27 15:58:22 -0500

    Summarzing feature added

--- analyzer.rb
+++ analyzer.rb
@@ -2,6 +2,7 @@ lines = File.readlines("text.txt")
 text = lines.join
 stopwords = %w{the a by on for of are with just but and to the my i has some in}
 
+# Main statistics
 line_count = lines.size
 total_characters = text.size
 total_characters_nospaces = text.gsub(/\s+/, '').size
@@ -20,3 +21,14 @@ puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
 puts "#{keywords_percentage}% of words are non-fluff words"
+
+puts
+
+# Summary
+sentences = text.gsub(/\s+/, ' ').strip.split(/\.|\?|!/)
+sentences_sorted = sentences.sort_by {|sentence| sentence.length}
+one_third = sentences_sorted.length / 3
+ideal_sentences = sentences_sorted.slice(one_third, one_third + 1)
+ideal_sentences = ideal_sentences.select {|sentence| sentence =~ /is|are/}
+
+puts ideal_sentences.join(". ")
